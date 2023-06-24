841b7bd820e8746a68e56d66ffba7027ee35cb08
trinhdangtinh <dangtinh908@gmail.com>
2023-06-24 23:12:57 +0700

    Create comment.php

---
+++ User/review/comment.php
@@ -0,0 +1,20 @@
+<?php
+require '../connection.php';
+if(isset($_POST["submit"])){
+    $id = $_POST["id"];
+      $name = $_POST['fullname'];
+      $email = $_POST["email"];
+      $comment = $_POST["comment"];
+  
+  
+      $query = "INSERT INTO comment VALUES('', '$name', '$email', '$comment', '$id')";
+      mysqli_query($conn, $query);
+      echo
+      "
+      <script>
+        alert('Successfully Added');
+        document.location.href = '../index.php';
+      </script>
+      ";
+    }
+?>
